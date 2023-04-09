91d5836085005be9bcdfe8b2b1d860d12530bb57
poa1024 <113357120+poa1024@users.noreply.github.com>
2023-04-09 11:38:00 +0200

    added ExplainTheGistAction

---
+++ src/main/java/io/github/poa1024/action/ExplainTheGistAction.java
@@ -0,0 +1,52 @@
+package io.github.poa1024.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import io.github.poa1024.Configuration;
+import io.github.poa1024.gpt.GptClient;
+import io.github.poa1024.gpt.GptQuestionBuilder;
+import org.jetbrains.annotations.NotNull;
+
+public class ExplainTheGistAction extends AnAction {
+
+    private final GptClient gptClient = Configuration.GPT_CLIENT;
+    private final GptQuestionBuilder gptQuestionBuilder = Configuration.GPT_QUESTION_BUILDER;
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+        var editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        var project = e.getRequiredData(CommonDataKeys.PROJECT);
+        var styleManager = CodeStyleManager.getInstance(project);
+        var documentManager = PsiDocumentManager.getInstance(project);
+        var document = editor.getDocument();
+        var psiFile = e.getData(LangDataKeys.PSI_FILE);
+
+        var caretModel = editor.getCaretModel();
+        var selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        var res = gptClient
+                .ask(gptQuestionBuilder.askForShortExplanation(selectedText))
+                .getFirstChoice();
+
+        int selectionStart = caretModel.getCurrentCaret().getSelectionStart();
+
+        WriteCommandAction.runWriteCommandAction(
+                project,
+                () -> {
+                    document.insertString(selectionStart - 1, res);
+                    documentManager.commitDocument(document);
+                    styleManager.reformatText(psiFile, selectionStart - 1, (selectionStart - 1) + res.length());
+                }
+
+
+        );
+        caretModel.getCurrentCaret().removeSelection();
+    }
+
+}
--- src/main/java/io/github/poa1024/gpt/GptQuestionBuilder.java
+++ src/main/java/io/github/poa1024/gpt/GptQuestionBuilder.java
@@ -3,7 +3,20 @@ package io.github.poa1024.gpt;
 public class GptQuestionBuilder {
 
     public String askForDetailedExplanation(String code) {
-        return "Explain the code. Do not change the code, just add explanatory comments. Your response should contains this code with your explanatory comments. Code starts here: " + code;
+        return "Explain the code. Do not change the code, just add explanatory comments. " +
+                "Your response should contains this code with your explanatory comments. " +
+                "Code starts here: " + code;
+    }
+
+    public String askForShortExplanation(String code) {
+        return "Give me a short explanation of what's happening in the code. " +
+                "Try to be concise. 3-4 sentences." +
+                "Wrap your answer as a comment block." +
+                "Comment should be in the style of the provided code." +
+                "Do not return anything except the comment block." +
+                "Your answer should not contain the code itself. Just the explanation." +
+                "Every new sentence should start with a new line." +
+                "Code starts here: " + code;
     }
 
 }
